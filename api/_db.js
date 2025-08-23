7800260e7aeba810da03a86d238cc5825439773c
chadilize4-creator <chadilize4@gmail.com>
2025-08-24 00:37:39 +0330

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -1,8 +1,10 @@
 // api/_db.js
 import pkg from 'pg';
+import { neon } from '@neondatabase/serverless';
 const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // needed for Neon in serverless
 });
+export const sql = neon(process.env.DATABASE_URL); // set in Vercel env
